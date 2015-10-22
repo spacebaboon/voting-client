31da8576d0ade25fdfee8688f0e78b213e5f14f4
Ben Avery <spacebaboon@gmail.com>
2015-10-22 08:49:04 +0100

    voting client complete.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
+import Winner from './Winner';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
+export const Results = React.createClass({
   mixins: [PureRenderMixin],
 
   getPair: function () {
@@ -36,5 +39,17 @@ export default React.createClass({
       </div>
     </div>;
   }
-
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  }
+}
+
+export const ResultsContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Results);
